8f375eda2a478dd371f2abf443c52c9917f13f43
Marie-Elise <membrq@gmail.com>
2017-06-15 22:41:04 -0500

    Assignment 1 - Intro to Data Structures

--- 01-data-structures/01-introduction-to-data-structures/line/line.rb
+++ 01-data-structures/01-introduction-to-data-structures/line/line.rb
@@ -1,4 +1,4 @@
-# A line of people at an amusement park ride
+  # A line of people at an amusement park ride
 # There is a front to the line, as well as a back.
 # People may leave the line whenever they see fit and those behind them take their place.
 class Line
@@ -6,29 +6,41 @@ class Line
 
   def initialize
     self.members = []
+
   end
 
   def join(person)
+    self.members << person
   end
 
   def leave(person)
+    self.members.delete(person)
+    ##move up people in the line?
   end
 
   def front
+    self.members.first
   end
 
   def middle
+    midpoint = (self.members.length/2).ceil
+    self.members[midpoint]
   end
 
   def back
+    self.members.last
   end
 
   def search(person)
+    if index(person)
+      person
+    end
   end
 
   private
 
   def index(person)
+    self.members.find_index(person)
   end
 
 end
--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -6,18 +6,35 @@ class Screen
   attr_accessor :matrix
 
   def initialize(width, height)
+    @width = width #columns
+    @height = height #rows
+    @matrix = Array.new(@height){Array.new(@width)}
   end
 
   # Insert a Pixel at x, y
   def insert(pixel, x, y)
+    @matrix[x][y] = pixel
   end
 
   def at(x, y)
+    #find pixel & return its x/y location
+    if inbounds(x,y)
+      return @matrix[x][y]
+    else
+      return nil
+    end
   end
 
   private
 
   def inbounds(x, y)
+    if (x < 0) || (y < 0)
+      return false
+    elsif (x > @width) || (y > @height)
+      return false
+    else
+      return true
+    end
   end
 
 end
