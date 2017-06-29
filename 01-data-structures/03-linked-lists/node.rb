85632533ab3e6be23145c923606b2991fc7e0352
Marie-Elise <membrq@gmail.com>
2017-06-28 22:16:45 -0500

    Assignment 3 Linked Lists completed

--- 01-data-structures/03-linked-lists/node.rb
+++ 01-data-structures/03-linked-lists/node.rb
@@ -3,5 +3,10 @@ class Node
   attr_accessor :data
 
   def initialize(data)
+    #@node = Node.new(data)
+    #is Node a data type?
+    @data = data
+    #@node = data
+    #@next = next
   end
 end
