96c5573adbda96cdf434d7ff09c3abfe62369906
Marie-Elise <membrq@gmail.com>
2017-07-04 16:28:45 -0500

    Assignment 4: Hashes Part I completed

--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -1,27 +1,59 @@
+require_relative 'hash_item'
+
 class HashClass
 
   def initialize(size)
     @items = Array.new(size)
   end
 
-  def []=(key, value)
+  def []=(key, value) #defines = operator
+    new_item = HashItem.new(key, value) #create new hash
+    i = index(key, size) #create i var to store index
+
+    if @items[i] === nil #if index === nil
+      @items[i] = new_item #assign new item to index
+    elsif @items[i].key != key #if key of index != key of new item
+      self.resize #resize
+      self[key] = value #pair value and key
+    elsif (@items[i].key === key) && (@items[i].value != value) #if collision = true & values are different
+      puts @items[i].value
+      puts value
+      self.resize #resize
+      @items[i] = value #pair key and value
+    end
   end
 
-
-  def [](key)
+  def [](key) #returns
+    if @items[index(key, size)] != nil && @items[index(key, size)].key === key
+      @items[index(key, size)].value
+    end
   end
 
-  def resize
+  def resize #double size of array
+    old_array = @items
+    @items = Array.new(old_array.length * 2)
+
+    old_array.each do |item|
+      unless item === nil #unless item is nil
+        self[item.key] = item.value #assign value to key
+      end
+    end
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    result = 0
+    key.each_byte do |letter| #converts characters to ascii values
+      result += letter #adds ascii values to result
+    end
+    result % size #mod results
   end
 
   # Simple method to return the number of items in the hash
   def size
+    @items.length
   end
 
 end
--- 01-data-structures/05-hashes-part-2/open_addressing/open_addressing.rb
+++ 01-data-structures/05-hashes-part-2/open_addressing/open_addressing.rb
@@ -2,26 +2,38 @@ require_relative 'node'
 
 class OpenAddressing
   def initialize(size)
+    @items = Array.new(size)
   end
 
   def []=(key, value)
   end
 
   def [](key)
+    @items[index(key, size)]
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    result = 0
+    key.each_byte do |letter| #converts characters to ascii values
+      result += letter #adds ascii values to result
+    end
+    result % size
   end
 
   # Given an index, find the next open index in @items
   def next_open_index(index)
+    #loop through @items or check index var only?
+    if index != nil
+      return -1
+    end
   end
 
   # Simple method to return the number of items in the hash
   def size
+    @items.length
   end
 
   # Resize the hash
